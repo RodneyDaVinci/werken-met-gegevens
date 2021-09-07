59c20316d2c3dea0a76b5254c934b4e26a7a1eb8
Rodney Vlot <99058248@mydavinci.nl>
2021-09-07 13:06:12 +0200

    Tweede commit!

---
+++ feestlunch.py
@@ -0,0 +1,7 @@
+Crossaintjes = 17
+Stokbrood = 2
+CrossaintPrijs = 0.39
+StokbroodPrijs = 2.78
+Korting = 1.50
+
+print( Crossaintjes * CrossaintPrijs + Stokbrood * StokbroodPrijs - Korting)
---
+++ speelhaldag.py
@@ -0,0 +1,5 @@
+Vrienden = 3
+Toegangsticket = 7.45
+PrijsGameSeat = 0.37
+TijdGameSeat = 9
+print(Vrienden * Toegangsticket + TijdGameSeat * PrijsGameSeat)
